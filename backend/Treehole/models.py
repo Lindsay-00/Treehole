f3b489fa07a3fe36ef04c102bfceae3473f34a61
vivsce <90162174+vivsce@users.noreply.github.com>
2021-12-07 00:14:37 +0000

    Tue, Dec 7, 2021, 12:14 AM +00:00

--- backend/Treehole/models.py
+++ backend/Treehole/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 # model for post
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['-created_on']
+    class Meta:
+        ordering = ['-created_on']
