cfa7bfdcbce35386e83e09e69f88fa6c5e0c3fd7
vivsce <90162174+vivsce@users.noreply.github.com>
2021-12-08 20:50:29 +0000

    Wed, Dec 8, 2021, 8:50 PM +00:00

--- myproject/flaskr/__init__.py
+++ myproject/flaskr/__init__.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required
+from myproject.flaskr.helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
