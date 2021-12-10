6d8092e96e11399b212a1a2cd6bb5844ed617e10
vivsce <90162174+vivsce@users.noreply.github.com>
2021-12-10 02:49:57 +0000

    Fri, Dec 10, 2021, 2:49 AM +00:00

--- FINAL_project/app.py
+++ FINAL_project/app.py
@@ -58,6 +58,7 @@ def comment():
     # get info from POST
     elif request.method == "POST":
         info = request.form
+        print(item for item in info)
         user_id = session["user_id"]
         post_name = request.form.get("post_id")
         content = request.form.get("reply")
